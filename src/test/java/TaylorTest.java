282e3ab30bb02409a1bfc4d8dcd7f614de0c6e2c
Gerunia <braineaterofficial7200@gmail.com>
2026-03-10 15:06:32 +0300

    task 1 update

--- src/test/java/TaylorTest.java
+++ src/test/java/TaylorTest.java
@@ -4,26 +4,21 @@ import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import ru.tpo.pikt.itmo.Taylor;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TaylorTest {
 
     @ParameterizedTest(name = "cos({0})")
     @DisplayName("check PI dots")
-    @ValueSource(doubles = {-2 * Math.PI, -Math.PI, -0.5 * Math.PI, 3 * Math.PI / 2, 2 * Math.PI})
+    @ValueSource(doubles = {-2, -1.5, -1, -0.5, 0, 0.5, 1.5, 1, 2.5, 2})
     void checkPiDots(double param) {
-        assertAll(
-                () -> assertEquals(Math.cos(param), Taylor.cos(param, 100), 0.0001)
-        );
+        assertEquals(Math.cos(param * Math.PI), Taylor.cos(param * Math.PI, 100), 0.0001);
     }
 
     @ParameterizedTest(name = "cos({0}) = {1}")
     @DisplayName("check values from table")
     @CsvFileSource(resources = "/cos_table.csv", numLinesToSkip = 1, delimiter = ';')
     void checkTable(double x, double y) {
-        assertAll(
-                () -> assertEquals(y, Taylor.cos(x, 100), 0.0001)
-        );
+        assertEquals(y, Taylor.cos(x, 100), 0.0001);
     }
 }
